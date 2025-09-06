6c18825f538c9493fcf90cbc68fa752fef421802
muhammed-alateeqi1 <mu.alateeqi@gmail.com>
2025-09-06 11:03:03 +0300

    first commit

---
+++ EvenOddChecker.js
@@ -0,0 +1,21 @@
+let userInteger;
+function evenOddChecker() {
+    let inputValue = document.querySelector('#evenOddInput').value.trim();
+    let evenOddOutput = document.querySelector('.evenOdd p');
+
+    if (inputValue === '') {
+        evenOddOutput.innerHTML = 'fill the field';
+        return;
+    }
+    userInteger = Number(inputValue);
+
+    if (isNaN(userInteger)) {
+        evenOddOutput.innerHTML = 'Please Enter A Number';
+    } else if (userInteger % 2 === 0) {
+        evenOddOutput.innerHTML = 'Is Even';
+        console.log(userInteger);
+
+    } else {
+        evenOddOutput.innerHTML = 'Is Odd';
+    }
+}
