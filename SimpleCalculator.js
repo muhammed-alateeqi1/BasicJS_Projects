fde17489fef65fe91d0498469bb2cb5933a01b99
muhammed-alateeqi1 <mu.alateeqi@gmail.com>
2025-09-08 05:56:01 +0300

    (feat): handle calculator buttons

---
+++ SimpleCalculator.js
@@ -0,0 +1,88 @@
+let one, two, three, four, five, six, seven, eight, nine, zero;
+
+
+function One() {
+    one = 1;
+    console.log(1);
+    document.getElementById("test").innerHTML += one;
+}
+function Two() {
+    two = 2;
+    console.log(two);
+    document.getElementById("test").innerHTML += two;
+}
+function Three() {
+    three = 3;
+    console.log(three);
+    document.getElementById("test").innerHTML += three;
+}
+function Four() {
+    four = 4;
+    console.log(four);
+    document.getElementById("test").innerHTML +=four;
+
+}
+function Five() {
+    five = 5;
+    console.log(five);
+    document.getElementById("test").innerHTML +=five;
+}
+function Six() {
+    six = 6;
+    console.log(six);
+    document.getElementById("test").innerHTML += six;
+}
+function Seven() {
+    seven = 7;
+    console.log(seven);
+    document.getElementById("test").innerHTML += seven;
+}
+function Eight() {
+    eight = 8;
+    console.log(eight);
+    document.getElementById("test").innerHTML += eight;
+
+}
+function Nine() {
+    nine = 9;
+    console.log(nine);
+    document.getElementById("test").innerHTML += nine;
+}
+function Zero() {
+    zero = 0;
+    console.log(zero);
+    document.getElementById("test").innerHTML += zero;
+}
+//----------------
+// Operators 
+//----------------
+
+function Divide() {
+    let divideOperator = '/';
+    console.log(divideOperator);
+    document.getElementById("test").innerHTML += divideOperator;
+}
+function Multiply() {
+    let multiplyOperator = '*';
+    console.log(multiplyOperator);
+    document.getElementById("test").innerHTML += multiplyOperator;
+}
+function Subtract() {
+    let subtractOperator = '-';
+    console.log(subtractOperator);
+    document.getElementById("test").innerHTML += subtractOperator;
+}
+function Plus() {
+    let plusOperator = '+';
+    console.log(plusOperator);
+    document.getElementById("test").innerHTML += plusOperator;
+}
+function Seperateor() {
+    let seperateorOperator = '.';
+    console.log(seperateorOperator);
+    document.getElementById("test").innerHTML += seperateorOperator;
+}
+function Equal() {
+    let seperateorOperator = '=';
+    console.log(seperateorOperator);
+}
